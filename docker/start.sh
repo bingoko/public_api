f113d94f0a3d5f53c8a4301fdd310194483f82ff
Zack Coburn <zack@zackcoburn.com>
2016-10-17 20:39:31 -0500

    update

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:80 -p 443:443 -v ~/storage_messagesCache:/public_api/storage_messagesCache --name etherdelta_api etherdelta/api
+docker run -d -p 80:80 -p 443:443 -v ~/public_api/storage_messagesCache:/public_api/storage_messagesCache --name etherdelta_api etherdelta/api
