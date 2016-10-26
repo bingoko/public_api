b7591d2773db9a1d856e3a17c0fe4abab5bfb5ba
Zack Coburn <zack@zackcoburn.com>
2016-10-25 23:45:37 -0500

    update

--- server.js
+++ server.js
@@ -65,28 +65,34 @@ app.use(function(err, req, res, next){
 
 function updateData() {
   API.logs(function(err, result){
-    try {
-      async.parallel(
-        [
-          function(callback) {
-            API.getOrders(function(err, result){
-              ordersData = {updated: Date.now(), result: result};
-              callback(null, undefined);
-            });
-          },
-          function(callback) {
-            API.returnTicker(function(err, result){
-              returnTickerData = {updated: Date.now(), result: result};
-              callback(null, undefined);
-            });
+    if (!err) {
+      try {
+        async.parallel(
+          [
+            function(callback) {
+              API.getOrders(function(err, result){
+                if (!err) {
+                  ordersData = {updated: Date.now(), result: result};
+                }
+                callback(null, undefined);
+              });
+            },
+            function(callback) {
+              API.returnTicker(function(err, result){
+                if (!err) {
+                  returnTickerData = {updated: Date.now(), result: result};
+                }
+                callback(null, undefined);
+              });
+            }
+          ],
+          function(err, result) {
+            setTimeout(updateData, 10*1000);
           }
-        ],
-        function(err, result) {
-          setTimeout(updateData, 10*1000);
-        }
-      );
-    } catch (err) {
-      console.log('err', err)
+        );
+      } catch (err) {
+        console.log('err', err)
+      }
     }
   });
 }
